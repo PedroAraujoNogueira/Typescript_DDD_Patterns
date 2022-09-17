3b5f9d102579c3b9956d6f7d28e69d0d26c09f1d
PedroAraujo <pedro.nogueira@contazap.com>
2022-09-16 23:08:34 -0300

    Finalizando o módulo de testes unitários

--- src/entity/order.spec.ts
+++ src/entity/order.spec.ts
@@ -22,18 +22,25 @@ describe("Order unit tests",() => {
 
     it("should calculate total.", () => {
         
-        const item = new OrderItem("1", "Arroz", 100);
-        const item2 = new OrderItem("1", "Feijão", 200);
+        const item = new OrderItem("1", "Arroz", 100, "produto1", 2);
+        const item2 = new OrderItem("1", "Feijão", 200, "produto2", 2);
         const order = new Order("o1", "c1", [item]);
 
         let total = order.total();
 
-        expect(total).toBe(100);
+        expect(total).toBe(200);
 
         const order2 = new Order("o2", "c2", [item, item2]);
 
         total = order2.total();
 
-        expect(total).toBe(300);
+        expect(total).toBe(600);
     });
+
+    it("should throw error if the item quantity is less or egual zero", () => {
+        expect(() => {
+            const item = new OrderItem("1", "item 1", 50, "Produto 1", 0); 
+            const order = new Order("1", "c1", [item])
+        }).toThrowError("Quantity must be greater than 0");
+    })
 });
--- src/entity/order_item.ts
+++ src/entity/order_item.ts
@@ -1,13 +1,25 @@
 export default class OrderItem {
 
-    _id: string;
-    _name: string;
-    _price: number;
+    private _id: string;
+    private _name: string;
+    private _price: number;
+    private _product: string;
+    private _quantity: number;
 
-    constructor(id: string, name: string, price: number){
+    constructor(id: string, name: string, price: number, product: string, quantity: number){
         this._id = id;
         this._name = name;
         this._price = price; 
+        this._product = product;
+        this._quantity = quantity;
+    }
+    
+    get quantity(): number {
+        return this._quantity;
+    }
+
+    get price(): number {
+        return this._price * this._quantity;
     }
 
 }
