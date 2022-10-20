2325078a094148216239633503e36d3dd3b66e0e
PedroAraujo <pedro.nogueira@contazap.com>
2022-10-19 22:08:28 -0300

    Finalizando implementação do CustomerRepository

---
+++ src/infrastructure/repository/order.repository.spec.ts
@@ -0,0 +1,36 @@
+import { Sequelize } from "sequelize-typescript"
+import Address from "../../domain/entity/address";
+import Customer from "../../domain/entity/customer";
+import CustomerModel from "../db/sequelize/model/customer.model";
+import OrderItemModel from "../db/sequelize/model/order-item.model";
+import OrderModel from "../db/sequelize/model/order.model";
+import ProductModel from "../db/sequelize/model/product.model";
+import CustomerRepository from "./customer.repository";
+
+describe("Customer repository test", () => {
+    let sequelize: Sequelize;
+
+    beforeEach(async () => {
+        sequelize = new Sequelize({
+            dialect: "sqlite",
+            storage: ":memory:",
+            logging: false,
+            sync: { force: true },
+        });
+        
+        sequelize.addModels([CustomerModel, OrderModel, OrderItemModel, ProductModel]);
+        await sequelize.sync();
+    });
+
+    afterEach(async () => {
+        await sequelize.close();
+    });
+
+    it("should create a new order", async () => {
+        const customerRepository = new CustomerRepository();
+        const customer = new Customer("123", "Customer 1");
+        const address = new Address("Street 1", 1, "Zipcode1", "City 1");
+        customer.changeAddress(address);
+        await customerRepository.create(customer);
+    })
+});
