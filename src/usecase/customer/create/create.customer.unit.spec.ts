0157794807107eaf48768961f0deef6a0279b51c
PedroAraujo <pedro.nogueira@contazap.com>
2022-12-07 23:18:53 -0300

    Criando use case para criar customer e implementando testes de criar customer quando temos dados invalidos

--- src/usecase/customer/create/create.customer.unit.spec.ts
+++ src/usecase/customer/create/create.customer.unit.spec.ts
@@ -1,3 +1,5 @@
+import CreateCustomerUseCase from "./create.customer.usecase";
+
 const MockRepository = () => {
     return {
         find: jest.fn(),
@@ -20,7 +22,7 @@ const input = {
 describe("Unit Test create customer use case", () => {
     it("should create a customer", async () => {
         const customerRepository = MockRepository();
-        const customerCreateUseCase = new CustomerCreateUseCase(customerRepository);
+        const customerCreateUseCase = new CreateCustomerUseCase(customerRepository);
 
         const output = await customerCreateUseCase.execute(input);
 
@@ -35,4 +37,28 @@ describe("Unit Test create customer use case", () => {
             },
         });
     });
+
+    it("should throw an error when name is missing", () => {
+        const customerRepository = MockRepository();
+        const customerCreateUseCase = new CreateCustomerUseCase(customerRepository);
+
+        input.name = "";
+        
+        expect(async ()=> {
+            await customerCreateUseCase.execute(input)
+        }).rejects.toThrow("Name is required");
+     
+    });
+    it("should throw an error when street is missing", () => {
+        const customerRepository = MockRepository();
+        const customerCreateUseCase = new CreateCustomerUseCase(customerRepository);
+
+        input.address.street = "";
+        
+        expect(async ()=> {
+            await customerCreateUseCase.execute(input)
+        }).rejects.toThrow("Street is required");
+     
+    })
+
 });
