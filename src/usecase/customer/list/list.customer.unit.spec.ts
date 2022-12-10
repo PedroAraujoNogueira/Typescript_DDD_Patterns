9fd065acef8c250dae373edb7407180af92d7ddd
PedroAraujo <pedro.nogueira@contazap.com>
2022-12-09 23:26:03 -0300

    Testando usecase para listagem de customer

---
+++ src/usecase/customer/list/list.customer.unit.spec.ts
@@ -0,0 +1,34 @@
+import CustomerFactory from "../../../domain/customer/factory/customer.factory";
+import Address from "../../../domain/customer/value-object/address";
+
+const customer1 = CustomerFactory.createWithAddress(
+    "Pedro", 
+    new Address("street 1", 1, "zip 1", "city 1")
+);
+
+const customer2 = CustomerFactory.createWithAddress(
+    "João", 
+    new Address("street 2", 2, "zip 2", "city 2")
+);
+
+const MockRepository = () => {
+    return {
+        create: jest.fn(),
+        update: jest.fn(),
+        find: jest.fn(),
+        findAll: jest.fn().mockReturnValue(Promise.resolve([customer1, customer2])),
+    }
+}
+
+describe("Unit test for listing customer use case", () => {
+    it("should list a customer", async () => {
+        const customerRepository = MockRepository();
+        const usecase = ListCustomerUseCase(customerRepository);
+
+        const output = await usecase.execute();
+
+        expect(output.customers.length).toBe(2);
+        expect(output.customers[0]).toEqual(customer1);
+        expect(output.customers[1]).toEqual(customer2);
+    })
+})
